d3cb676678cacb6a4c7017621e3a981cb0d46ad0
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-11-23 17:29:05 -0500

    implementacion de interfaz login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Pizarras UX-1.0</h1>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { PiCloverDuotone } from "react-icons/pi";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { LuUser } from "react-icons/lu";
+import { TbPasswordFingerprint } from "react-icons/tb";
+
+type Inputs = {
+  username: string;
+  pass: string;
+};
+const LoginForm = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const [showPassword, setShowPassword] = useState(false);
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <div className="w-full md:w-[600px] z-10 m-2 bg-[#161616fb] rounded-lg flex justify-center items-center flex-col">
+        <div className="text-green-600 mt-2">
+          <PiCloverDuotone className="h-[60px] w-[60px]" />
+        </div>
+        <div className="text-[#FFF113] font-lilita text-[34px]">
+          El tebrol de la suerte
+        </div>
+        <div className="text-[#FFF113]">Inicia sesión para acceder a tu cuenta</div>
+        <form
+          className="w-full flex flex-col"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="px-8 space-y-6 mt-6">
+            <div className="w-full flex flex-col gap-1 ">
+              <div className="text-white font-semibold">Usuario</div>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <LuUser className="h-5 w-5 text-gray-400" />
+                </div>
+
+                <input
+                  type="text"
+                  placeholder="Usuario"
+                  className="appearance-none block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent transition-all"
+                  {...register("username")}
+                />
+              </div>
+            </div>
+            {/*Contrase'a*/}
+            <div className="w-full flex flex-col gap-1 ">
+              <div className="text-white font-semibold">contraseña</div>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <TbPasswordFingerprint className="h-5 w-5 text-gray-400" />
+                </div>
+
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="••••••••"
+                  className="appearance-none block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent transition-all"
+                  {...register("pass")}
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <FiEyeOff className="h-5 w-5 text-gray-400 hover:text-gray-600" />
+                  ) : (
+                    <FiEye className="h-5 w-5 text-gray-400 hover:text-gray-600" />
+                  )}
+                </button>
+              </div>
+            </div>
+            <div className="pb-6 pt-2">
+            <button type="submit" className="w-full h-[42px] bg-green-700 rounded-lg text-white">
+              Iniciar sesion
+            </button>
+            </div>
+            
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+export default LoginForm;
