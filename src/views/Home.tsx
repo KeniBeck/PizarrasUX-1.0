1517f865a0d589c5858fc9dbddda46b0b7f1dc80
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-11-26 21:21:02 -0500

    ajustes de estilos en index.html

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,13 @@
-import { GiClover } from "react-icons/gi"
+import { GiClover } from "react-icons/gi";
 import LoginForm from "../components/LoginForm";
-const Home = () =>{
-    return(
+const Home = () => {
+  return (
     <>
-    <div className="w-full h-screen bg-[#262626] flex justify-center items-center relative">
-    <GiClover className="w-[400px] h-[400px] fixed left-[-135px] top-[-60px] text-[#1d1d1d]"/>
-    <LoginForm/>
-    </div>
+      <div className="w-full h-full bg-[#262626] flex justify-center items-center relative">
+        <GiClover className="w-[400px] h-[400px] fixed left-[-135px] top-[-60px] text-[#1d1d1d]" />
+        <LoginForm />
+      </div>
     </>
-    );
-}
+  );
+};
 export default Home;
